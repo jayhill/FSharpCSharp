c0bf750008494fce83c9093fddc41c4798fa8530
Jay Hill <jayhill@jayhill.net>
2015-10-17 09:34:26 -0400

    added FSharp.Data type provider

--- FSharpCSharp/CSharp/ScratchPad.cs
+++ FSharpCSharp/CSharp/ScratchPad.cs
@@ -5,7 +5,5 @@ using FSharp.Common;
 namespace CSharp { partial class Program { static partial void Demo()
 {
 
-    var x = ExpressionsAndQuotations.squareExpr.Compile().Invoke(3);
-    Console.WriteLine(x);
 
 } } }
